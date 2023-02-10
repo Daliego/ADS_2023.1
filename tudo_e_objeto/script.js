65ca49fd6a801683a2c93d4594b7c58ebc2d1e87
Diego de Sousa Araujo <diesgo.araujo@gmail.com>
2023-02-10 14:25:37 -0300

    Novas pastas criadas

---
+++ tudo_e_objeto/script.js
@@ -0,0 +1,23 @@
+import react from 'react'
+import axios from 'axios'
+/* class Menu {
+    constructor(menu) {
+        this.menuElement = document.querySelector(menu)
+    }
+    addActiveElement() {
+        this.menuElement.addActiveElement("clik", event)
+        event.target.classlist.add("active")
+    }
+}
+
+const menu = new Menu(".principal")
+
+console.log(menu)
+ */
+function myButton() {
+    return (
+        <button>
+            Click!
+        </button>
+    )
+}
